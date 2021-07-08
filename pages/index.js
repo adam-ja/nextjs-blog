64a47d611f86ef85b3691ee527b9a24efc7e270a
Adam Auskerin <adam.auskerin@gmail.com>
2021-07-08 18:03:57 +0100

    Completed dynamic routes

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import Date from '../components/date';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
@@ -13,33 +15,35 @@ export async function getStaticProps() {
 }
 
 export default function Home({ allPostsData }) {
-  return (
-      <Layout isHome>
-          <Head>
-              <title>{siteTitle}</title>
-          </Head>
-          <section className={utilStyles.headingMd}>
-              <p>[Your Self Introduction]</p>
-              <p>
-                  (This is a sample website - you’ll be building a site like this on{' '}
-                  <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-              </p>
-          </section>
+    return (
+        <Layout isHome>
+            <Head>
+                <title>{siteTitle}</title>
+            </Head>
+            <section className={utilStyles.headingMd}>
+                <p>[Your Self Introduction]</p>
+                <p>
+                    (This is a sample website - you’ll be building a site like this on{' '}
+                    <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+                </p>
+            </section>
 
-          <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-              <h2 className={utilStyles.headingLg}>Blog</h2>
-              <ul className={utilStyles.list}>
-                  {allPostsData.map(({ id, date, title }) => (
-                      <li className={utilStyles.listItem} key={id}>
-                          {title}
-                          <br />
-                          {id}
-                          <br />
-                          {date}
-                      </li>
-                  ))}
-              </ul>
-          </section>
-      </Layout>
-  )
+            <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+                <h2 className={utilStyles.headingLg}>Blog</h2>
+                <ul className={utilStyles.list}>
+                    {allPostsData.map(({ id, date, title }) => (
+                        <li className={utilStyles.listItem} key={id}>
+                            <Link href={`/posts/${id}`}>
+                                <a>{title}</a>
+                            </Link>
+                            <br />
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date} />
+                            </small>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </Layout>
+    )
 }
